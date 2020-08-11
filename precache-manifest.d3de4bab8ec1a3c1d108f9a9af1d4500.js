3365c6189883aa3ecd2d298150ca4566fdb290d5
AndreaL <alafertteo@gmail.com>
2020-08-11 13:18:04 -0400

    Updates

---
+++ precache-manifest.d3de4bab8ec1a3c1d108f9a9af1d4500.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651273fe26c78ff1c0f9eb6c48a59a38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f39ad6372b8343b45c4",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "47b06eacc734ce3a136b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f39ad6372b8343b45c4",
+    "url": "/static/js/2.42417c42.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.42417c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b06eacc734ce3a136b",
+    "url": "/static/js/main.d1f1783b.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  },
+  {
+    "revision": "d4430e3106a223c202422859edb490f6",
+    "url": "/static/media/discord.d4430e31.png"
+  }
+]);
