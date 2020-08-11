8f1448387d9c8a5c9699745846536ca68e987b64
AndreaL <alafertteo@gmail.com>
2020-08-11 13:25:56 -0400

    Updates

---
+++ precache-manifest.9e8c2e7a47b71e3ea825a46997613c3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08237d29cba0cb1bf3bc56a91b92bec",
+    "url": "/ficfinder/index.html"
+  },
+  {
+    "revision": "8f39ad6372b8343b45c4",
+    "url": "/ficfinder/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "47b06eacc734ce3a136b",
+    "url": "/ficfinder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f39ad6372b8343b45c4",
+    "url": "/ficfinder/static/js/2.42417c42.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/ficfinder/static/js/2.42417c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b06eacc734ce3a136b",
+    "url": "/ficfinder/static/js/main.d1f1783b.chunk.js"
+  },
+  {
+    "revision": "87e16dc4862ec9c9901f",
+    "url": "/ficfinder/static/js/runtime-main.5801a6e3.js"
+  },
+  {
+    "revision": "d4430e3106a223c202422859edb490f6",
+    "url": "/ficfinder/static/media/discord.d4430e31.png"
+  }
+]);
